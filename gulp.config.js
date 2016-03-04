59a1da9a3ef961975101164f5463f21ead9fb3ff
Byron Sommardahl <byron@acklenavenue.com>
2016-03-04 11:53:41 -0600

    Adding coverage tool to gulp.

--- gulp.config.js
+++ gulp.config.js
@@ -4,7 +4,8 @@ module.exports = function() {
 		util: {
 			sevenZip: "C:/Program Files/7-Zip/7z.exe",
 			msbuild: "C:/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe",
-			mspec: process.cwd() + "/lib/Machine.Specifications.Runner.Console.0.9.1/tools/mspec-clr4.exe"			
+			mspec: process.cwd() + "/lib/Machine.Specifications.Runner.Console.0.9.2/tools/mspec-clr4.exe",
+			openCover: process.cwd() + "/lib/OpenCover.4.6.519/tools/OpenCover.Console.exe"
 		},
 		appVersion: process.env["APPVEYOR_BUILD_VERSION"] || "local",
 		buildPath: process.cwd() + '/build',
