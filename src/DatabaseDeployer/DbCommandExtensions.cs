323dc9991d264a8efd5167ee771398f5b2cad7fa
Byron Sommardahl <byron@acklenavenue.com>
2016-08-11 17:40:21 -0500

    Disposing reader properly.

--- src/DatabaseDeployer/DbCommandExtensions.cs
+++ src/DatabaseDeployer/DbCommandExtensions.cs
@@ -7,10 +7,12 @@ namespace DatabaseDeployer
     {
         public static void ExecuteSqlFile(this IDbCommand cmd, string filename)
         {
-            var tr = new StreamReader(filename);
-            string sql = tr.ReadToEnd();
-            cmd.CommandText = sql;
-            cmd.ExecuteNonQuery();
+            using (var tr = new StreamReader(filename))
+            {
+                string sql = tr.ReadToEnd();
+                cmd.CommandText = sql;
+                cmd.ExecuteNonQuery();
+            }
         }
     }
 }
