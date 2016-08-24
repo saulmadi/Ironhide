c6bd453dc56d3357b966f5d747fea5989a1a5f7b
Lerik <jj.cob@unitec.edu>
2016-08-24 08:06:52 -0600

    Testing if events library is being retrieved

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,9 @@ var path = require('path');
 var merge = require('merge-stream');
 var rename = require('gulp-rename');
 var glob = require('glob');
-require('events').EventEmitter.defaultMaxListeners = Infinity;
+var events = require('events');
+console.log(events);
+events.EventEmitter.defaultMaxListeners = Infinity;
 
 var sonarQubeUtil = require('./sonarQubeAppVeyorUtil')(shell, {
         sonarRunnerDownloadPath: config.sonarRunnerDownloadPath,
